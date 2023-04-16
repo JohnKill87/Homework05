b6f75a9ce4533562bd519e0de40dc955c8ec604f
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-04-16 15:39:45 +0300

    Решение Домашнего задания 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,91 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        int iOS = 0;
+        int android = 1;
+        if (clientOS == android) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2015;
+        int clientOS = 1;
+        int iOS = 0;
+        int android = 1;
+        if (clientOS == android && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == iOS && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == android && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == iOS && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int year = 2021;
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int deliveryDaysTime;
+        if (deliveryDistance <= 20){
+            deliveryDaysTime = 1;
+            System.out.println("Потребуется дней: " + deliveryDaysTime);
+        } else if (deliveryDistance >= 20 && deliveryDistance <= 60){
+            deliveryDaysTime = 2;
+            System.out.println("Потребуется дней: " + deliveryDaysTime);
+        } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            deliveryDaysTime = 3;
+            System.out.println("Потребуется дней: " + deliveryDaysTime);
+        } else {
+            System.out.println("Доставки нет");
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println(monthNumber + " Месяц принадлежит сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(monthNumber + " Месяц принадлежит сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println(monthNumber + " Месяц принадлежит сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println(monthNumber + " Месяц принадлежит сезону осень");
+                break;
+            default:
+                System.out.println("Номер месяца больше 12");
+        }
     }
 }
